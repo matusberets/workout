3b7e10ba0d7de0814740952df23dc37359faf424
matusberets <matusberets@gmail.com>
2021-01-11 21:42:38 +0100

    update

--- application.py
+++ application.py
@@ -36,7 +36,7 @@ Session(app)
 # db = SQL("sqlite:///workout.db")
 
 # To connect to a Postgresql heroku database
-db = SQL(os.getenv("postgres://jorqzsdckjpref:e757bbed8d7f33357c6c52e446df4b9863300b89ad7cdfbee42682a247e1e4cd@ec2-52-211-161-21.eu-west-1.compute.amazonaws.com:5432/d5gpufg0ht2tcv"))
+db = SQL(os.environ.get("postgres://jorqzsdckjpref:e757bbed8d7f33357c6c52e446df4b9863300b89ad7cdfbee42682a247e1e4cd@ec2-52-211-161-21.eu-west-1.compute.amazonaws.com:5432/d5gpufg0ht2tcv"))
 
 #global variable list for storing chosen picture
 chosen_exercise = []
